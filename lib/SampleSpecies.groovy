f3fa24d33a59ca23eb4da4c263a7987302ea8e6a
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-07-22 18:20:00 +0000

    Clean up

--- lib/SampleSpecies.groovy
+++ lib/SampleSpecies.groovy
@@ -1,12 +1,10 @@
 import groovy.transform.Immutable
-import groovy.transform.Sortable
 import nextflow.io.ValueObject
 import nextflow.util.KryoHelper
 import nextflow.util.ArrayBag
 
 @ValueObject
 @Immutable(copyWith=true, knownImmutables = ['id','ploidy','kmer_size'])
-//@Sortable(includes = ['id'])
 class SampleSpecies {
     static { 
         // Register this class with the Kryo framework that serializes and deserializes objects
@@ -17,28 +15,6 @@ class SampleSpecies {
     String id
     int ploidy
     int kmer_size
-
-    // SampleSpecies(String name, int ploidy, int kmer_size){
-    //     id = name
-    //     this.ploidy = ploidy
-    //     this.kmer_size = kmer_size
-    // }
-
-    boolean equals(Object o){
-        if ( o.is(this) )
-            return true;
-
-        if (!(o instanceof SampleSpecies))
-            return false;
-
-        try {
-            return o.id.equals(id);
-        } catch (ClassCastException e)   {
-            return false;
-        } catch (NullPointerException e) {
-            return false;
-        }
-    }
 
     public String toString(){
         return "[ id: $id, ploidy: $ploidy, kmer_size: $kmer_size ]"
--- main.nf
+++ main.nf
@@ -4,6 +4,8 @@ workflow {
     Channel.of( 
         [ new SampleSpecies(id:'hsapiens',ploidy:2,kmer_size:31), 'A' ], 
         [ new SampleSpecies(id:'hsapiens',ploidy:2,kmer_size:31), 'B' ] 
-    ).groupTuple()
+    )   
+        .view { species, letter -> species.id } 
+        .groupTuple()
         .view()
 }
