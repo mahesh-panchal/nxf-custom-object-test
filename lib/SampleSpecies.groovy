4cb809b8659c25a74cca8d0f92b2c0761938e2aa
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2022-07-22 14:35:49 +0000

    Add SampleSpecies class

---
+++ lib/SampleSpecies.groovy
@@ -0,0 +1,42 @@
+import groovy.transform.Immutable
+import nextflow.io.ValueObject
+import nextflow.util.KryoHelper
+import nextflow.util.ArrayBag
+
+@ValueObject
+@Immutable(copyWith=true, knownImmutables = ['id','ploidy','kmer_size'])
+class SampleSpecies {
+    static { 
+        // Register this class with the Kryo framework that serializes and deserializes objects
+        // that pass through channles. This allows for caching when this object is used.
+        KryoHelper.register(SampleSpecies)
+    }
+
+    String id
+    int ploidy
+    int kmer_size
+
+    SampleSpecies(String name, int ploidy, int kmer_size){
+        id = name
+        this.ploidy = ploidy
+        this.kmer_size = kmer_size
+    }
+
+    @override
+    boolean equals(Object o){
+        if ( o.is(this) )
+            return true;
+
+        if (!(o instanceof SampleSpecies))
+            return false;
+
+        try {
+            return o.id.equals(id);
+        } catch (ClassCastException e)   {
+            return false;
+        } catch (NullPointerException e) {
+            return false;
+        }
+    }
+
+}
